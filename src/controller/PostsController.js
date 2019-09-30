c738d1e0ca557a79d1ff6083025e7d8b57935a00
Gaston Graciani <Gastongraciani@gmail.com>
2019-09-29 23:16:33 -0300

    Get posts and tested

--- src/controller/PostsController.js
+++ src/controller/PostsController.js
@@ -16,5 +16,17 @@ module.exports = {
       res.status(400).json(error);
       res.end();
     }
+  },
+
+  getAllPosts: async (req, res, next) => {
+    try {
+      const posts = await Post.find();
+      if (posts.length === 0) {
+        return await res.status(200).json({ collection: "empty" });
+      }
+      return res.status(200).json(posts);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
   }
 };
--- test/posts-api/Posts.js
+++ test/posts-api/Posts.js
@@ -1,6 +1,6 @@
 const expect = require("chai").expect;
 const request = require("supertest");
-const chaiHttp = require("chai-http");
+const Post = require("../../src/models/postSchema");
 const { app } = require("../../src/server");
 const db = require("../../src/db");
 
@@ -8,7 +8,7 @@ const database = new db();
 
 const post = {};
 
-describe("post /notes", () => {
+describe("post /notes", async () => {
   before(done => {
     database
       .startConnection()
@@ -38,10 +38,9 @@ describe("post /notes", () => {
           clicks: 0
         }
       })
-      .set("Accept", "application/json")
       .then(res => {
-        expect(res.statusCode).to.equal(201);
         expect(res.body).to.contain.property("_id");
+        expect(res.statusCode).to.equal(201);
         done();
       })
       .catch(err => done(err));
@@ -63,12 +62,32 @@ describe("post /notes", () => {
           clicks: 0
         }
       })
-      .set("Accept", "application/json")
       .then(res => {
-        expect(res.statusCode).to.equal(400);
         expect(res.body).to.contain.property("error");
+        expect(res.statusCode).to.equal(400);
         done();
       })
       .catch(err => done(err));
+  });
+
+  it("Getting all posts", done => {
+    request(app)
+      .get("/api/posts")
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        Post.collection.drop();
+        done();
+      });
+  });
+
+  it("Getting none posts", done => {
+    request(app)
+      .get("/api/posts")
+      .then(res => {
+        expect(res.body).to.contain.property("collection")
+        expect(res.statusCode).to.equal(200);
+        Post.collection.drop();
+        done();
+      });
   });
 });
